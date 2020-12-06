c1f09205fd8648539c2e55ae2d73769942efd859
Margaret Elson <margaret.a.elson@gmail.com>
2020-12-06 14:37:32 -0500

    Got current time and date to work

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-$("#currentDay").text(luxon.DateTime.local().toLocaleString({
-    weekday: 'long', month: 'long', day: '2-digit' 
+$("#currentDay").text(
+    luxon.DateTime.local().toLocaleString({
+    weekday: 'long', 
+    month: 'long', 
+    day: '2-digit', 
 }))
 
+let currentTime = luxon.DateTime.local().toLocaleString({hour: "2-digit"});
+console.log(currentTime)
+
+
+
+
+
 //Store
-localStorage.setItem("lastname", "smith");
-//Retrieve
-document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+// localStorage.setItem("lastname", "smith");
+// //Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
 
 var hour = $(".hour");
 var timeBlock = $(".time-block");
 var saveBtn = $(".saveBtn");
 
+
 var agenda = [];
 
-init();
+// init();
 
-function init(){
-    var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
+// function init(){
+//     var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
 
-    if(storedAgenda !== null) {
-        agenda = storedAgenda;
-    }
-//render agenda to the DOM
-    renderAgenda();
-}
+//     if(storedAgenda !== null) {
+//         agenda = storedAgenda;
+//     }
+// //render agenda to the DOM
+//     renderAgenda();
+// }
 
-function renderAgenda() {
-    timeBlock.innerHTML = "";
-}
+// function renderAgenda() {
+//     timeBlock.innerHTML = "";
+// }
